91478a3fb91032ccf0677acfb6a3a096cf76ae50
George Chitu <doru.chitu96@gmail.com>
2026-04-24 16:03:20 +0300

    add front-end pack and test for Docker

---
+++ FrontEnd/server.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import cors from 'cors';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const app = express();
+
+app.use(cors());
+app.use(express.static('public'));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log('Server running on http://localhost:${PORT}');
+});
